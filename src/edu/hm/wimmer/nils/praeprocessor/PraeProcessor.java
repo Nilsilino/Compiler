76c6e6d4939182f6222446b40c3a6eb8b722634d
Nilsilino <nils.wimmer@hotmail.com>
2018-03-29 10:03:42 +0200

    aktuelle fertige Version

--- src/edu/hm/wimmer/nils/praeprocessor/PraeProcessor.java
+++ src/edu/hm/wimmer/nils/praeprocessor/PraeProcessor.java
@@ -6,10 +6,11 @@ import edu.hm.cs.rs.compiler.toys.base.Source;
 
 public class PraeProcessor implements Preprocessor {
 	
-	private State currentState;
+	
+	private int zeilen = 0;
 	
 	enum State {
-		START, SLASH, SLASH_SLASH, SLASH_STAR, SLASH_STAR_STAR, SLASH_STAR_STAR_SLASH
+		START, SLASH, SLASH_SLASH, SLASH_STAR, SLASH_STAR_STAR, SLASH_STAR_STAR_SLASH,ZEILEN_KOMMENTAR,BLOCK_KOMMENTAR,KEIN_KOMMENTAR
 	}
 	
 	public PraeProcessor() {
@@ -19,7 +20,9 @@ public class PraeProcessor implements Preprocessor {
 	@Override
 	public Source process(Source unprocess) throws LexicalError {
 		// TODO Auto-generated method stub
+		State currentState;
 		String combined = "";
+		String beforBlock = "";
 		Source process = new Source(); 
 		currentState = State.START;
 		while (unprocess.hasMore()) {
@@ -28,13 +31,20 @@ public class PraeProcessor implements Preprocessor {
 			case '/':
 				if (currentState == State.SLASH) {
 					currentState = State.SLASH_SLASH;
+					beforBlock = combined;
+					break;
 				} else if (currentState == State.SLASH_SLASH) {
+					combined = combined + character;
 					break;
 				} else if (currentState == State.SLASH_STAR_STAR) {
-					currentState = State.SLASH_STAR_STAR_SLASH;
+					currentState = State.START;
+					combined = beforBlock + " ";
+					break;
 				} else {
+					beforBlock = combined;
 					currentState = State.SLASH;
 				}
+				combined = combined + character;
 				break;
 			case '*':
 				if (currentState == State.SLASH) {
@@ -42,9 +52,36 @@ public class PraeProcessor implements Preprocessor {
 				} else if (currentState == State.SLASH_STAR) {
 					currentState = State.SLASH_STAR_STAR;
 				}
+				combined = combined + character;
 				break;
-				
+			case '\n':
+				if (currentState == State.START) {
+					//System.out.println(combined);
+					combined += character;
+					process.append(combined);
+					combined = "";
+					break;
+				} else if ( currentState == State.SLASH_SLASH) {
+					// Hier fehlt noch Zeilenübersetzung
+					zeilen++;
+					combined = "\n";
+					process.append(combined);
+					currentState = State.START;
+					break;
+				} else {
+					// Hier fehlt noch Zeilenübersetzung
+					combined = "\n";
+					process.append(combined);
+					zeilen++;
+					break;
+				}	
+			default:
+				if (currentState == State.SLASH) {
+					currentState = State.START;
+				}
+				combined = combined + character;
 			}
+			
 		}
 		return process;
 	}
