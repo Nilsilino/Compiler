fe69f277606212a5fabe2ac5d3e793887c370d29
Nilsilino <nils.wimmer@hotmail.com>
2018-04-25 11:46:10 +0200

    Unfertig

--- src/src/edu/hm/wimmer/nils/sprachgenerator/Sprachgenerator.java
+++ src/src/edu/hm/wimmer/nils/sprachgenerator/Sprachgenerator.java
@@ -2,6 +2,7 @@ package src.edu.hm.wimmer.nils.sprachgenerator;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import edu.hm.cs.rs.compiler.lab04generator.LanguageGenerator;
@@ -12,12 +13,32 @@ public class Sprachgenerator implements LanguageGenerator{
 	private String [] splittedString;
 	
 	public static void main(String... args) {
-		parseGrammar1("=;S=(S)S;S=()").map(Arrays::deepToString).forEach(System.out::println);;
+		LanguageGenerator generator = new Sprachgenerator();
+		double start = System.currentTimeMillis();
+		System.setProperty("prettyprint", "blaBLABLAJDFKALNKL");
+		Stream grammar = generator.parseGrammar("=;S=(S)S;S=()");
+		grammar = LanguageGenerator.prettyprint(grammar);
 	}
-	
+	   
+	/** Generiert alle Woerter der Sprache der Grammatik bis zur gegebenen Laenge.
+     * Start ist die linke Seite der ersten Produktion.
+     * @param grammar Typ-1-Grammatik.
+     * @param uptoLength Maximale Laenge der Woerter der Sprache der Grammatik.
+     * @return Alle Woerter der Sprache bis (einschliesslich) zur Laenge uptoLength,
+     * sortiert nach steigender Laenge und bei gleicher Laenge alphabetisch.
+     */
 	@Override
 	public Stream<String> generate(Stream<String[]> grammar, int uptoLength) {
 		// TODO Auto-generated method stub
+		List <String[]> grammatik = grammar.collect(Collectors.toList());
+		String start = "";
+		for (String[] s : grammatik) {
+			if (start == "") {
+				start = s[0];
+			} else {
+				
+			}
+		}
 		return null;
 	}
 	
@@ -35,10 +56,5 @@ public class Sprachgenerator implements LanguageGenerator{
 		
 	}
 	
-	public static Stream<String[]> parseGrammar1(String grammarString) {
-		String zeichenTrenner = grammarString.substring(0, 1);
-		String produktionsTrenner = grammarString.substring(1, 2);
-		return Stream.of(grammarString.split(produktionsTrenner)).skip(1).map(productionString -> productionString.split(zeichenTrenner));
-		
-	}
+
 }
